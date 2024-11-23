47b17dc9d4e17f24fd8d6174f69b81b12903ee79
Jadest <Jadest13x03@gmail.com>
2024-11-23 19:01:28 +0900

    123

---
+++ svg.d.ts
@@ -0,0 +1,9 @@
+// svg.d.ts
+declare module '*.svg' {
+  import React from 'react';
+  const src: string;
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+  export default src;
+}
