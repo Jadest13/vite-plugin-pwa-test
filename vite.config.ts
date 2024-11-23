df9194a0827c954fb1d8b567a37c7edd7ed9d93a
Jadest <Jadest13x03@gmail.com>
2024-11-23 19:08:53 +0900

    123

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     svgr(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
       // workbox: {
       //   clientsClaim: true,
       //   skipWaiting: true,
